80a7932e3d5cf5f0c4e4d0ff38364c227769e6ba
Jean Kang <jeankang.dev@gmail.com>
2021-03-09 03:52:27 -0500

    HTML & CSS

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+// const exphbs = require('express-handlebars');
+// app.engine('.hbs', exphbs({ extname: '.hbs' }));
+// app.set('view engine', '.hbs');
+
+app.use(express.static('./'));			// or ./views, ./public
+
+app.get('/', (req, res) => {
+  res.send('word');
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)
+});
