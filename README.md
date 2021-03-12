30d18ba60412b110c0e4dd0cb1d96116fef31334
Jean Kang <jeankang.dev@gmail.com>
2021-03-12 02:03:37 -0500

    README: update

---
+++ README.md
@@ -0,0 +1,85 @@
+## 😼 PROJECT
+
+A cat album to practice fetching API data and rendering it
+
+---
+
+## 📎 PLAN
+
+#### STACK
+
+- Node.js, HTML, CSS, Vanilla JavaScript(ES6)
+
+#### FEATURE
+
+- [x] fetch API data
+- [ ] render sub-data when clicking directories / files
+- [ ] update Breadcrumb on click
+- [ ] render data on Breadcrumb click
+- [ ] display back button in sub-directories
+- [ ] display cat image with modal when clicking files
+- [ ] close modal when clicking outside of the modal or ESC
+
+#### OTHERS
+
+- [ ] use Component  
+       App and other components  
+       each component is independent from each other
+- [ ] API call  
+       loose coupling  
+       error check
+      inform the user that the error occured  
+       use async, or await for practice
+- [ ] minimize global scope pollution
+- [ ] minimize event binding
+- [ ] use cash to render data that has been rendered already
+
+#### STEP
+
+- render the application to main.js that has class App
+- index.html has 3 sections
+
+#### markup:
+
+1.  Breadcrumb: show current directory (root)
+
+          <nav class="Breadcrumb">
+            <div>root</div>
+            <div>yellow cat</div>
+          </nav>
+
+2.  Nodes: render data for current directory
+
+- node type: DIRECTORY
+
+          <div class="Node">
+            <img src="./assets/directory.png">
+            <div>2021/04</div>
+          </div>
+
+- node type: FILE
+
+          <div class="Node">
+            <img src="./assets/file.png">
+            <div>yawning cat</div>
+          </div>
+
+- display back-button if the current directory is not 'root'
+
+          <div class="Node">
+            <img src="./assets/prev.png">
+          </div>
+
+  3.  ImageView: render file image on modal when clicking files
+
+            <div class="ImageViewer">
+              <div class="content">
+                <img src="">
+              </div>
+            </div>
+
+---
+
+## 💡 PROBLEM SOLVING HISTORY
+
+- [ ] 2020/01/05 : Dashboard - Check in / Check out date not matching issue
--- main.js
+++ main.js
@@ -1,3 +1,112 @@
 'use strict';
 
-console.log('work');
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch('https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev')
+  // return fetch('./data/data.json')
+    .then((response) => response.json())
+    .then((json) => json);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+  const currentFolder = document.querySelector('.current__folder');
+  if (currentFolder.innerText != 'root') {
+    const itemList = document.querySelector('ul');
+    itemList.innerHTML = items.map((item) => createHTMLString(item)).join('');
+    // displayBackButton(); 
+  }
+  const itemList = document.querySelector('ul');
+  itemList.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  let result;
+  if(item.type === "DIRECTORY") {
+    result = `<li class="album__content" data-key="id" data-id="${item.id}">
+      <i class="far fa-folder-open"></i>
+      <span class="description">${item.name}</span>
+      <span style="visibility='hidden'" parent="${parent}"></span>
+    </li>`;
+  } else if (item.type === "FILE") {
+    result = `<li class="album__content" data-key="id" data-id="${item.id}">
+      <img src="${item.img}"></img>
+      <span class="description">${item.name}</span>
+      <span style="visibility='hidden'" filePath="${item.filePath}" parent="${parent}"></span>
+    </li>`;
+  }
+  return result;
+}
+
+function setEventListeners(items) {
+  let root = document.querySelector('.current__folder');
+  let rootText = root.innerText;
+  console.log(rootText);
+  root.addEventListener('click', () => {
+    rootText = 'root';
+    displayItems(items);
+  });
+
+  const folders = document.querySelector('.album__contents ul');
+  folders.addEventListener('click', (event) => {
+    let target;
+    if (event.target == null) {
+      return;
+    } 
+    
+    if (event.target.tagName != 'div') {
+      target = event.target.parentNode;
+    } else {
+      target = event.target;
+    }
+    console.log(target);
+
+    fetchMatchingItems(target)
+      .then((items) => {
+        updateCurrentFolder(event);
+        // displayBackButton();
+        displayItems(items);
+        setEventListeners(items);
+      })
+      .catch(console.log);
+  });
+}
+
+function fetchMatchingItems(target) {
+  console.log(target);
+  console.log(target.dataset.key);    // "id"
+  console.log(target.dataset.id);  // "#id number"
+  const id = target.dataset.id;
+
+  // const id = event.target.data.value;
+  return fetch(`https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev/:${id}`)
+    .then(response => response.json())
+    .then(json => json);
+}
+
+function updateCurrentFolder(event) {
+  const currentFolder = document.querySelector('.current__folder');
+  const folderName = event.target.parentNode.innerText;
+  currentFolder.innerHTML = `root - ${folderName}`;
+}
+
+function displayBackButton() {
+  const list = document.querySelector('.album__contents ul');
+  let backButton = document.createElement('li');
+  backButton.classList.add('btn back');
+  backButton.innerHTML = `<i class="fas fa-chevron-left"></i>`;
+
+  list.insertBefore(backButton, list.firstChild);
+  backButton.addEventListener('click', () => {
+    window.history.back();
+  });
+}
+
+// main
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
