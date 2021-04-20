b04199905795bb876032100a4fb830eb5351b95a
Jean Kang <jeankang.dev@gmail.com>
2021-04-20 06:02:59 -0400

    JS: create App Component

---
+++ src/components/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// function App is in between function Breadcrumb and Nodes
+function App(app) {
+  this.state = {
+    isRoot: false,
+    nodes: [],
+    depth: []
+  }
+
+  const breadcrumb = new Breadcrumb({
+    app,
+    initialState: this.state.depth
+  })
+
+  const nodes = new Nodes({
+    app,
+    initialState: {
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes
+    },
+
+    // When clicking events happen in Nodes, call onClick function
+    // In this way, Nodes don't have to know what logic the onClick func has
+    onClick: (node) => {
+      if (node.type === 'DIRECTORY') {
+        // deal with Breadcrumb here
+      } else if (node.type === 'FILE') {
+      }
+    }
+  })
+  
+}
